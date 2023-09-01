0fc603cc2b7d69bbe2231a3cc56b00220db8c9db
siam786e <shohrab.hossain36@gmail.com>
2023-09-01 21:16:32 +0600

    add footer

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
   import Navbar from './components/Navbar.vue'
+  import Footer from './components/Footer.vue'
 </script>
 
 <template>
   <navbar></navbar>
+  <Footer></Footer>
 </template>
 
 <style scoped>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -16,7 +16,7 @@
 </script>
 
 <template>
-    <nav class="bg-gray-800">
+    <nav class="bg-emerald-900">
         <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div class="relative flex h-16 items-center justify-between">
                 <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
@@ -25,10 +25,10 @@
                             <h1 class="text-xl font-bold text-white">News Bangla</h1>
                         </router-link>
                     </div>
-                    <div class="sm:ml-6 sm:block">
+                    <div class="sm:ml-6 sm:block">  
                         <div class="flex space-x-4">
                             <div v-for="(category, index) in categories" :key="index"
-                                class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium cursor-pointer"
+                                class="text-white hover:bg-red-700 hover:text-white rounded-md px-3 py-3 text-sm font-medium cursor-pointer"
                                 aria-current="page">
                                 <router-link :to="'/category/' + category.id">
                                     {{ category.name }}
